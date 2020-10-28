6ab59f327806c27e359d3997798455c07724042f
chenglei <chenglei@4366.com>
2020-10-28 21:25:24 +0800

    RSA 工具类添加

--- account-admin/src/main/java/org/account/cl/config/AdminJwtLoginFilter.java
+++ account-admin/src/main/java/org/account/cl/config/AdminJwtLoginFilter.java
@@ -9,8 +9,10 @@ import org.account.cl.permissions.impl.TokenServiceImpl;
 import org.account.cl.view.product.JsonView;
 import org.account.cl.view.product.RetUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -23,12 +25,14 @@ import java.io.*;
  * 用户登陆的过滤器
  * @author Administrator
  */
-@Component
+@Configuration
 @Order(10)
 public class AdminJwtLoginFilter extends OncePerRequestFilter {
 
     private final static String LOGIN_URL = "/admin/login.do";
     private final static String REQUEST_TYPE = "POST";
+    private final static String PASSWORD_MD5_KEY = "d021cd2e28637dee3e9331d9de3cf311";
+    public final static String PASSWORD_PREFIX = "1qaz2wsx3edc4rfv!@#$%^&**TWDFHAJBFHFJHAJWRFA";
 
     @Autowired
     private UserService userService;
@@ -48,29 +52,33 @@ public class AdminJwtLoginFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // 登陆请求处理
-        if (LOGIN_URL.equals(request.getRequestURI()) && REQUEST_TYPE.equals(request.getMethod())) {
-            // 取得用户信息
-            InputStream inputStream = request.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            String jsonStr = reader.readLine();
-            String username = null;
-            if (JcStringUtils.isNotBlank(jsonStr)) {
-                JSONObject jsonObj = JSON.parseObject(jsonStr);
-                username = jsonObj.getString("username");
-                if (JcStringUtils.isEmpty(username)) {
-                    username = "";
-                }
-                String password = jsonObj.getString("password");
-                if (JcStringUtils.isEmpty(password)) {
-                    password = "";
-                }
+        end: if (LOGIN_URL.equals(request.getRequestURI()) && REQUEST_TYPE.equals(request.getMethod())) {
+            try {
+                // 取得用户信息
+                InputStream inputStream = request.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                String jsonStr = reader.readLine();
 
-                // 进行用户身份认证
-                if (attachUser(username, password)) {
-                    successHandler(username, response);
+                if (JcStringUtils.isNotBlank(jsonStr)) {
+                    JSONObject jsonObj = JSON.parseObject(jsonStr);
+                    String username = jsonObj.getString("username");
+                    if (JcStringUtils.isEmpty(username)) {
+                        username = "";
+                    }
+                    String password = jsonObj.getString("password");
+                    if (JcStringUtils.isEmpty(password)) {
+                        password = "";
+                    }
+
+                    // 进行用户身份认证 用户名和密码去掉首尾空格
+                    if (attachUser(username.trim(), password.trim())) {
+                        successHandler(username, response);
+                        break end;
+                    }
                 }
-            }
-            failureHandler(username, response);
+            // 参数信息错误
+            } catch (Exception ignored) {}
+            failureHandler(response);
         } else {
             // 其它请求放行
             filterChain.doFilter(request, response);
@@ -79,10 +87,8 @@ public class AdminJwtLoginFilter extends OncePerRequestFilter {
 
     private boolean attachUser(String username, String password) {
         User user = userService.getUserByUsername(username);
-        if (user != null && password.equals(user.getPassword())) {
-            return true;
-        }
-        return false;
+        password = PASSWORD_PREFIX + password;
+        return user != null && getPasswordEncoder().matches(password, user.getPassword());
     }
 
     /**
@@ -92,29 +98,32 @@ public class AdminJwtLoginFilter extends OncePerRequestFilter {
      */
     private void successHandler(String username, HttpServletResponse response) {
         String token = tokenService.generateToken(username);
-        sendMsg(token, response);
+        sendMsg(new JsonView.JsonRet(token), response);
     }
 
     /**
      * 登陆认证失败的处理
-     * @param username
      * @param response
      */
-    private void failureHandler(String username, HttpServletResponse response) {
-        String msg = username + "用户认证失败!";
-        sendMsg(msg, response);
+    private void failureHandler(HttpServletResponse response) {
+        sendMsg(new JsonView.JsonRet(401, "认证失败，用户名或密码有误！"), response);
     }
 
-    private void sendMsg(String msg, HttpServletResponse response) {
+    private void sendMsg(JsonView.JsonRet jsonRet, HttpServletResponse response) {
         response.setContentType(RetUtils.CONTENT_TYPE_JSON);
-        JsonView.JsonRet jsonRet = new JsonView.JsonRet(msg);
         String res = JSON.toJSONString(jsonRet);
         try (Writer out = response.getWriter();) {
             response.setContentLength(res.getBytes(RetUtils.CHARACTER_CODE).length);
             out.write(res);
             out.flush();
         } catch (Exception e) {
+            // 其实不会走到的
             e.printStackTrace();
         }
+    }
+
+    @Bean
+    public BCryptPasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
